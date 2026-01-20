1297667cdbe0bfcefdee2939d49fbb5a8711bd81
MYQ <jerry200223200223@126.com>
2026-01-20 15:51:56 +0800

    搭建项目框架

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import './assets/main.css'
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App";
+import "./assets/css/main.css";
+import router from "./router";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { fileURLToPath, URL } from "node:url";
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss(), Icons()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+    strictPort: true,
+  },
+});
